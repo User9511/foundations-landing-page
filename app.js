c692148150573c43efa3d98f74d526ac851cda4e
Smgy94 <Shanemcgeown@gmail.com>
2023-07-06 21:53:23 +0100

    Added Transition Animation to Mobile Menu
    Mobile Menu Animates onto the screen from the top down underneath the nav.

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
+// Get the button element
 const mobileMenuTrigger = document.getElementById("mobileMenuTrigger");
-const navWrapper = document.getElementById("navWrapper");
-const navLinks = document.getElementById("nav-links");
-const bars = document.querySelectorAll(".bar");
 
+// Add a click event listener to the button
 mobileMenuTrigger.addEventListener("click", function () {
-  const expanded = mobileMenuTrigger.getAttribute("aria-expanded") === "true";
-  mobileMenuTrigger.setAttribute("aria-expanded", !expanded);
+  // Toggle the aria-expanded attribute value
+  const isExpanded = mobileMenuTrigger.getAttribute("aria-expanded") === "true";
+  mobileMenuTrigger.setAttribute("aria-expanded", !isExpanded);
+
+  // Toggle the active class on the navWrapper and nav-links elements
+  const navWrapper = document.getElementById("navWrapper");
+  const navLinks = document.getElementById("nav-links");
+  const bars = document.querySelector(".bar");
   navWrapper.classList.toggle("active");
   navLinks.classList.toggle("active");
-  bars.forEach((bar) => bar.classList.toggle("active"));
+  bars.classList.toggle("active");
 });
