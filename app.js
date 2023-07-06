2223777f933c646709b237db5e23ab9499daa3e8
Smgy94 <Shanemcgeown@gmail.com>
2023-07-06 16:19:14 +0100

    Toggle Aria Expanded Attribute
    Update the aria-expanded value attribute when the user has clicked on the hamburger menu

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const mobileMenuTrigger = document.getElementById("mobileMenuTrigger");
 const navWrapper = document.getElementById("navWrapper");
 const navLinks = document.getElementById("nav-links");
+const bar = document.querySelector(".bar");
 
 mobileMenuTrigger.addEventListener("click", function () {
   navWrapper.classList.toggle("active");
   navLinks.classList.toggle("active");
+  bar.classList.toggle("active");
 });
+
+// Need to change mobilemenu aria-expanded to true when the menu is expanded
