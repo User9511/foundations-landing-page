4c0a367166a847b63e9e0ce0e6a533199f9d5802
Smgy94 <Shanemcgeown@gmail.com>
2023-07-06 16:26:55 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const mobileMenuTrigger = document.getElementById("mobileMenuTrigger");
 const navWrapper = document.getElementById("navWrapper");
 const navLinks = document.getElementById("nav-links");
-const bar = document.querySelector(".bar");
+const bars = document.querySelectorAll(".bar");
 
 mobileMenuTrigger.addEventListener("click", function () {
+  const expanded = mobileMenuTrigger.getAttribute("aria-expanded") === "true";
+  mobileMenuTrigger.setAttribute("aria-expanded", !expanded);
   navWrapper.classList.toggle("active");
   navLinks.classList.toggle("active");
-  bar.classList.toggle("active");
+  bars.forEach((bar) => bar.classList.toggle("active"));
 });
-
-// Need to change mobilemenu aria-expanded to true when the menu is expanded
